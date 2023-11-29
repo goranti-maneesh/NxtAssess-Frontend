ad727e07d00f5b7596b688bc85129ba6ce72a7b2
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2023-11-29 18:23:36 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx,css}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.vue",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
